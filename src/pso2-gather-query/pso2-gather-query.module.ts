f8d47efeef7506ae70d32089f6273403dc608ebe
Loc Nguyen <nghloc1603@gmail.com>
2020-07-08 16:58:01 +0700

    update resource query

--- src/pso2-gather-query/pso2-gather-query.module.ts
+++ src/pso2-gather-query/pso2-gather-query.module.ts
@@ -5,10 +5,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ModuleNameEnums } from 'src/shared/module_name.enum';
 import { GatherResourceSchema } from 'src/shared/schemas/gather-resource.schema';
 import { GatherCuisineSchema } from 'src/shared/schemas/gather-cuisine.schema';
+import { GatherCraftSchema } from 'src/shared/schemas/gather-craft.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: ModuleNameEnums.gather_resource, schema: GatherResourceSchema }, {name: ModuleNameEnums.gather_cuisine, schema: GatherCuisineSchema}]),
+    MongooseModule.forFeature([
+      { name: ModuleNameEnums.gather_resource, schema: GatherResourceSchema },
+      { name: ModuleNameEnums.gather_cuisine, schema: GatherCuisineSchema },
+      { name: ModuleNameEnums.gather_craft, schema: GatherCraftSchema}
+    ]),
   ],
   controllers: [GatherQueryController],
   providers: [GatherQueryService],
