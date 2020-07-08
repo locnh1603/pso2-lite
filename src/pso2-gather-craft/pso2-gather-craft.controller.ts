ed1ed73f61bd39cc706196338d7caae183aa34c1
Loc Nguyen <nghloc1603@gmail.com>
2020-07-08 16:50:24 +0700

    finish crafting module

--- src/pso2-gather-craft/pso2-gather-craft.controller.ts
+++ src/pso2-gather-craft/pso2-gather-craft.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, UseGuards, Body } from '@nestjs/common';
+import { ApiResponse, ApiBody } from '@nestjs/swagger';
+import { GatherCraft } from 'src/shared/schemas/gather-craft.schema';
+import { GatherCraftService } from 'src/pso2-gather-craft/pso2-gather-craft.service';
+import { GatherCraftDto } from 'src/shared/dto/gather-craft-dto.model';
+import { JwtAuthGuard } from 'src/pso2-admin-auth/guard/auth.guard';
 
 @Controller('gather-crafts')
-export class GatherCraftController { }
+export class GatherCraftController { 
+  constructor(private craftService: GatherCraftService) {}
+
+  @Get()
+  @ApiResponse({status: 201, type: [GatherCraft]})
+  findAll(): Promise<GatherCraft[]> {
+    return this.craftService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  @ApiBody({type: GatherCraftDto})
+  @ApiResponse({status: 201, type: GatherCraft})
+  create(@Body() newCraft: GatherCraftDto) {
+    return this.craftService.create(newCraft);
+  }
+}
