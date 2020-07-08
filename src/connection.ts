a6dd465445bef81ae2b5d25dbd268b28890a444d
Loc Nguyen <nghloc1603@gmail.com>
2020-07-08 15:59:46 +0700

    hot fix

--- src/connection.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose'
-import { logger } from 'src/shared/middleware/logger';
-
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-this.db = mongoose.connection.on('error', () => {
-  logger.error(`database connection error on url ${process.env.DB_URL}`)
-}).once('open', () => {
-  logger.info(`database connection success on url ${process.env.DB_URL}`)
-});
