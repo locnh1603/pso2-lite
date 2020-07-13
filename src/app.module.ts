fc6f7cb95c0312957d36ff821c23362ac7dc18ce
Loc Nguyen <nghloc1603@gmail.com>
2020-07-13 16:44:30 +0700

    remove redundant decclarations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { GatherResourceModule } from 'src/pso2-gather-resource/pso2-gather-resou
 import { GatherQueryModule } from 'src/pso2-gather-query/pso2-gather-query.module';
 import { GatherCuisineModule } from 'src/pso2-gather-cuisine/pso2-gather-cuisine.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Pso2AdminUserModule } from './pso2-admin-user/pso2-admin-user.module';
-import { Pso2AdminAuthModule } from './pso2-admin-auth/pso2-admin-auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { LoggerMiddleware, AuthLoggerMiddleware } from 'src/shared/middleware/logger.middleware';
+import { LoggerMiddleware } from 'src/shared/middleware/logger.middleware';
 import { GatherCuisineController } from 'src/pso2-gather-cuisine/pso2-gather-cuisine.controller';
 import { GatherResourceController } from 'src/pso2-gather-resource/pso2-gather-resource.controller';
 import { GatherQueryController } from 'src/pso2-gather-query/pso2-gather-query.controller';
@@ -15,8 +13,6 @@ import { GatherCraftModule } from 'src/pso2-gather-craft/pso2-gather-craft.modul
 import { RequestValidatorGuard } from 'src/shared/guards/request-validator.guard';
 import { RequestDataNameTransformPipe, RequestParamNameTransformPipe } from 'src/shared/pipes/request-name-data-transform.pipe';
 import { RequestRecipeGuard } from 'src/shared/guards/request-recipe.guard';
-import { AdminUserController } from 'src/pso2-admin-user/pso2-admin-user.controller';
-import { Pso2AdminAuthController } from 'src/pso2-admin-auth/pso2-admin-auth.controller';
 
 @Module({
   imports: [
