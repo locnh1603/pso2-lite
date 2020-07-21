ac63f45fc3fed7313667ceee07b08beae8478182
Loc Nguyen <nghloc1603@gmail.com>
2020-07-21 15:44:48 +0700

    update URI

--- src/shared/environments/environments.ts
+++ src/shared/environments/environments.ts
@@ -1,3 +1,3 @@
 export const environments = {
-  mongodb_uri: 'mongodb://admin:locnh1603@ds163656.mlab.com:63656/heroku_1qn3p0n8'
+  mongodb_uri: 'mongodb+srv://rw_user:rw_user@cluster0.rqq8l.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
